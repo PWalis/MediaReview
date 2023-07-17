ed8db3d16b55c06e129a959949d39048741db9e8
PWalis <pwilky1016@gmail.com>
2023-07-16 20:03:02 -0700

    added express js backend

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/api', (req, res) => {
+    res.json({"users": ["user1", "user2"]});
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
