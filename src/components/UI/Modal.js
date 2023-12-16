d67381f2e5153151ab14e18feeded6dcc74ad958
PWalis <pwilky1016@gmail.com>
2023-12-15 17:27:13 -0700

    the damn modal in safari

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import react from "react";
 
 const Modal = (props) => {
     return (
-        <div className="fixed flex z-50 bg-gray-800 bg-opacity-60 w-screen h-screen pb-52">
+        <div className="fixed flex z-50 md:bg-gray-800 bg-opacity-60 w-screen h- pb-52">
             <div  className="m-auto bg-white rounded-lg shadow-lg p-10 z-20">
                 {props.children}
             </div>
