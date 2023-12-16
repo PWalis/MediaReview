1ac4551b2601b212d517484508614a2cbd4165e7
PWalis <pwilky1016@gmail.com>
2023-12-15 17:05:09 -0700

    more respnsive

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import react from "react";
 
 const Modal = (props) => {
     return (
-        <div className="fixed flex z-10 bg-gray-800 bg-opacity-60 w-screen h-screen pb-52">
+        <div className="fixed flex z-50 bg-gray-800 bg-opacity-60 w-screen h-screen pb-52">
             <div  className="m-auto bg-white rounded-lg shadow-lg p-10 z-20">
                 {props.children}
             </div>
